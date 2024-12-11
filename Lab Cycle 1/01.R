6436dda2d2852ad07e574aa1979eff602e350209
Aswin_SuresH <117917680+aswinsuresh-04@users.noreply.github.com>
2024-12-11 23:54:59 +0530

    Update 01.R

--- Lab Cycle 1/01.R
+++ Lab Cycle 1/01.R
@@ -1,47 +1,28 @@
-para <- function() {
-  cat("Enter your paragraph:\n")
-  paragraph <- readline()
-  
-  words <- unlist(strsplit(paragraph, "\\s+"))
-  
-  # (a)
-  words_number <- length(words)
-  cat("\nThe total number of words =", words_number, "\n")
-  
-  # (b)
-  word_len <- nchar(words)
-  avg_word_len <- mean(word_len)
-  cat("Average word length =", avg_word_len, "\n")
-  
-  # (c)
-  longest_len <- max(word_len)
-  longest_word <- words[word_len == longest_len]
-  # longest_word <- longest_words[1] # In case there are multiple words with the same length
-  cat("The longest word is:", longest_word, "\n")
-  
-  # (d)
-  cat("Enter the word to be replaced: ")
-  replace <- readline()
-  cat("Enter the word to replace with: ")
-  new <- readline()
-  
-  modified_para <- modified(replace, new, paragraph)
-  cat("The modified paragraph is:\n", modified_para, "\n")
+analyze_text <- function() {
+# Input the paragraph
+cat("Enter your paragraph:\n")
+paragraph <- readline()
+# Split the paragraph into words
+words <- unlist(strsplit(paragraph, "\\s+"))
+# a. Count the total number of words
+total_words <- length(words)
+cat("\nThe total number of words =", total_words, "\n")
+# b. Calculate the average word length
+word_lengths <- nchar(words)
+avg_word_length <- mean(word_lengths)
+cat("Average word length =", avg_word_length, "\n")
+# c. Identify the longest word
+longest_word <- words[which.max(word_lengths)]
+cat("The longest word is:", longest_word, "\n")
+# d. Replace a word
+cat("Enter the word to replace: ")
+target_word <- readline()
+cat("Enter the replacement word: ")
+replacement_word <- readline()
+# Replace the word in the paragraph
+modified_paragraph <- gsub(paste0("\\b", target_word, "\\b"),
+replacement_word, paragraph, ignore.case = TRUE)
+cat("\nThe modified paragraph is:\n", modified_paragraph, "\n")
 }
-
-modified <- function(replace, new, paragraph) {
-  words <- unlist(strsplit(paragraph, "\\s+"))
-  
-  mod <- sapply(words, function(word) {
-    if (word == replace) {
-      return(new)
-    } else {
-      return(word)
-    }
-  })
-  
-  modified_para <- paste(mod, collapse = " ")
-  return(modified_para)
-}
-
-para()
+# Run the function
+analyze_text()
